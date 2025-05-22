73f508f5512ab9d44db4d29b6d100e6ab7a50b27
alvinChristianto <alvinchristianto93@gmail.com>
2025-05-22 23:28:22 +0700

    adding widget stat for pendapatan laundry and service ac

---
+++ app/Filament/Widgets/AcReminderOverview.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\AcWorkingReport;
+use App\Models\LaundryTransaction;
+use Carbon\Carbon;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Filament\Widgets\TableWidget as BaseWidget;
+use Illuminate\Database\Eloquent\Builder; // <--- ADD THIS LINE
+
+class AcReminderOverview extends BaseWidget
+{
+   
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(function (Builder $query) {
+
+
+                $startOfMonth = Carbon::now()->startOfMonth();
+                $endOfMonth = Carbon::now()->endOfMonth();
+
+                // Return an Eloquent query for BakpiaTransaction model
+                // Filter transactions created within the current month
+                return AcWorkingReport::query()
+                    ->whereBetween('created_at', [$startOfMonth, $endOfMonth])
+                    ->orderBy('created_at', 'desc'); // Order by creation date, newest first
+                // tidak ada filter dan data tidak muncul di widget ac reminder
+            })
+            ->columns([
+                Tables\Columns\TextColumn::make('id_report')
+                    ->label('Id'),
+                Tables\Columns\TextColumn::make('customer.name')
+                    ->label('Klien')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('customer.phone_number')
+                    ->label('No.telp'),
+                Tables\Columns\TextColumn::make('next_service_date')
+                    ->label('tgl service selanjutnya')
+                    ->date()
+                    ->sortable(),
+            ])
+            ->filters([
+                Tables\Filters\SelectFilter::make('month')
+                    ->options([
+                        1 => 'January',
+                        2 => 'February',
+                        3 => 'March',
+                        4 => 'April',
+                        5 => 'May',
+                        6 => 'June',
+                        7 => 'July',
+                        8 => 'August',
+                        9 => 'September',
+                        10 => 'October',
+                        11 => 'November',
+                        12 => 'December',
+                    ])
+                    ->query(function (Builder $query, array $data): Builder {
+                        // Only apply the month filter if a month is selected
+                        return $query->when(
+                            $data['value'], // The selected month number (1-12)
+                            fn (Builder $query, $month) => $query->whereMonth('next_service_date', $month)
+                        );
+                    })
+                    ->label('Filter by Month')
+                    ->default(Carbon::now()->month),
+            ]);
+    }
+}
---
+++ app/Filament/Widgets/AcWorkStatOverview.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\AcWorkingReport;
+use Carbon\Carbon;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class AcWorkStatOverview extends BaseWidget
+{
+
+    protected function getStats(): array
+    {
+        $now = Carbon::now()->format('Y-m-d');
+        // Mendapatkan tanggal saat ini
+        $today = Carbon::now()->format('Y-m-d');
+        $currentMonth = Carbon::now()->month;
+        $currentYear = Carbon::now()->year;
+
+        // Menghitung pendapatan HARIAN
+        $RawdailyRevenue = AcWorkingReport::query()
+            ->whereDate('created_at', $today)
+            ->sum('total_price');
+
+        // Menghitung pendapatan BULANAN
+        $RawmonthlyRevenue = AcWorkingReport::query()
+            ->whereMonth('created_at', $currentMonth)
+            ->whereYear('created_at', $currentYear) // Pastikan tahun juga sesuai
+            ->sum('total_price');
+
+        // Menghitung pendapatan TAHUNAN
+        $RawyearlyRevenue = AcWorkingReport::query()
+            ->whereYear('created_at', $currentYear)
+            ->sum('total_price');
+
+
+        $dailyRevenue = 'Rp ' . number_format($RawdailyRevenue, 2, '.', '');
+        $monthlyRevenue = 'Rp ' . number_format($RawmonthlyRevenue, 2, '.', '');
+        $yearlyRevenue = 'Rp ' . number_format($RawyearlyRevenue, 2, '.', '');
+
+
+        return [
+            Stat::make('Pend. SERVICE AC HARIAN', $dailyRevenue)
+                ->description('Pendapatan Service AC Per Hari ini ' . Carbon::now()->format('d-m-Y'))
+                ->descriptionIcon('heroicon-m-arrow-trending-up')
+                // ->url(route('filament.admin.resources.reservations.index'))
+                ->color('primary'),
+            Stat::make('Pend. SERVICE AC BULANAN', $monthlyRevenue)
+                ->description('Pendapatan Service AC Per Bulan ' . Carbon::now()->format('M'))
+                ->descriptionIcon('heroicon-m-arrow-trending-up')
+                // ->url(route('filament.admin.resources.reservations.index'))
+                ->color('info'),
+            Stat::make('Pend. SERVICE AC TAHUNAN', $yearlyRevenue)
+                ->description('Pendapatan Service AC pada tahun ' . $currentYear)
+                ->descriptionIcon('heroicon-m-arrow-trending-up')
+                // ->url(route('filament.admin.resources.reservations.index'))
+                ->color('warning'),
+        ];
+    }
+}
