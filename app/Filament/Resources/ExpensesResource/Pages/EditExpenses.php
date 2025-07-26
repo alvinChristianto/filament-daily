c6671f6b59a70c50065028e0a3ec7e1597fa18d3
alvinChristianto <alvinchristianto93@gmail.com>
2025-07-26 17:22:30 +0700

    adding afterSave logic for expenses to edit daily revenue expenses too

--- app/Filament/Resources/ExpensesResource/Pages/EditExpenses.php
+++ app/Filament/Resources/ExpensesResource/Pages/EditExpenses.php
@@ -6,7 +6,11 @@ use App\Filament\Resources\ExpensesResource;
 use App\Models\DailyRevenueExpenses;
 use App\Models\Expenses;
 use Filament\Actions;
+use Filament\Actions\Action; // Import Action for custom actions if needed
+use Filament\Actions\EditAction;
+use Filament\Actions\SaveAction;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class EditExpenses extends EditRecord
 {
@@ -18,8 +22,40 @@ class EditExpenses extends EditRecord
             Actions\DeleteAction::make()
                 ->after(function (Expenses $record) {
                     $idToDelete = $record->id_expenses;
-                    DailyRevenueExpenses::where('id_transaction', $idToDelete )->delete();
+                    DailyRevenueExpenses::where('id_transaction', $idToDelete)->delete();
                 }),
+
         ];
+    }
+
+    protected function afterSave(): void
+    {
+        $updateData = $this->data;
+
+        $drCash = 0;
+        $drNonCash = 0;
+        $crCash = 0;
+        $crNonCash = 0;
+
+        if ($updateData["id_payment"] == 1) {
+            $crCash = $updateData["price_total"];
+        } else {
+            $crNonCash = $updateData["price_total"];
+        }
+
+        DailyRevenueExpenses::where('id_transaction', $updateData['id_expenses'])
+            ->update([
+                'title' => $updateData['title'],
+                'payment_category' => $updateData["id_payment"],
+                // 'dr_cash' => $drCash,            //no need to update since it is expenses
+                // 'dr_noncash' =>  $drNonCash,
+                'cr_cash' => str_replace(',', '', $crCash),
+                'cr_noncash' =>  str_replace(',', '', $crNonCash),
+                'expense_other' =>  str_replace(',', '', $updateData["price_total"])
+            ]);
+    }
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
     }
 }
