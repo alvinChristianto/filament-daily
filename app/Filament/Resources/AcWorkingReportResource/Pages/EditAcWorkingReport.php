66821b862f5aaeebf2efa71b9e7a0f412f6c7a7f
alvinChristianto <alvinchristianto93@gmail.com>
2025-08-26 09:34:59 +0700

    adding logic edit service ac will reflected to dashboard dailyrevenue

--- app/Filament/Resources/AcWorkingReportResource/Pages/EditAcWorkingReport.php
+++ app/Filament/Resources/AcWorkingReportResource/Pages/EditAcWorkingReport.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\AcWorkingReportResource\Pages;
 
 use App\Filament\Resources\AcWorkingReportResource;
+use App\Models\DailyRevenueExpenses;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 
@@ -14,6 +15,46 @@ class EditAcWorkingReport extends EditRecord
     {
         return [
             Actions\DeleteAction::make(),
+            
+            /**not yet use */
+            // Actions\DeleteAction::make()
+            //     ->after(function (Expenses $record) {
+            //         $idToDelete = $record->id_expenses;
+            //         DailyRevenueExpenses::where('id_transaction', $idToDelete)->delete();
+            //     }),
+
         ];
+    }
+
+    protected function afterSave(): void
+    {
+        $updateData = $this->data;
+        // dd($updateData);
+        $drCash = 0;
+        $drNonCash = 0;
+        $crCash = 0;
+        $crNonCash = 0;
+
+        if ($updateData["id_payment"] == 1) {
+            $drCash = $updateData["total_price"];
+        } else {
+            $drNonCash = $updateData["total_price"];
+        }
+
+        DailyRevenueExpenses::where('id_transaction', $updateData['id_report'])
+            ->update([
+                'title' => $updateData['title'],
+                'payment_category' => $updateData["id_payment"],
+                'dr_cash' => $drCash,            
+                'dr_noncash' =>  $drNonCash,
+                // 'cr_cash' => str_replace(',', '', $crCash), //no need to update since the case is pendapatan
+                // 'cr_noncash' =>  str_replace(',', '', $crNonCash),
+                'revenue_serviceac' =>  str_replace(',', '', $updateData["total_price"])
+            ]);
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
     }
 }
