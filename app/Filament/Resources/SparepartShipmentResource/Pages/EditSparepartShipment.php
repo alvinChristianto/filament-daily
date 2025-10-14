3d5f49a958eabb245fe1383cad5f918decd3583e
alvinChristianto <alvinchristianto93@gmail.com>
2025-10-14 22:37:02 +0700

    feat: adding feature update sparepart shipment affect sparepart stock

--- app/Filament/Resources/SparepartShipmentResource/Pages/EditSparepartShipment.php
+++ app/Filament/Resources/SparepartShipmentResource/Pages/EditSparepartShipment.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\SparepartShipmentResource\Pages;
 
 use App\Filament\Resources\SparepartShipmentResource;
+use App\Models\SparepartStock;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 
@@ -15,5 +16,40 @@ class EditSparepartShipment extends EditRecord
         return [
             Actions\DeleteAction::make(),
         ];
+    }
+
+    // protected function mutateFormDataBeforeSave(array $data): array
+    // {
+    //     $this->data = $data;
+    //     dd($data);
+    //     return $data;   
+    // }
+
+    protected function afterSave(): void
+    {
+        //20251014 update sparepart stock if there is any changes in the sparepart transaction details
+        $updateData = $this->data;
+        $sparepartStockToUpdate = $updateData['transaction_detail'];
+        $idToUpdate = $updateData['id_transaction'] ;
+        $trxDate = $updateData['transaction_date'] ;
+
+        foreach ($sparepartStockToUpdate as $key => $value) {
+            SparepartStock::where('id_transaction', $updateData['id_transaction'])
+                ->update([
+                    'id_warehouse' => '1', //$res["id_warehouse"], //$res['id_outlet'], //lol
+                    'id_sparepart' => $value["id_sparepart"],
+                    'id_transaction' => $idToUpdate,
+                    'status' => 'STOCK_SOLD_MAINSTORE',     //stock sold from mainstore 
+                    'amount' =>  $value["sent_stock"],
+                    'description' => "",
+                    'stock_record_date' => $trxDate,
+                ]);
+            # code...
+        }
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
     }
 }
