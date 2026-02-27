df267d2152821ee680ca80a8b36a0c7c145e011f
alvinChristianto <alvinchristianto93@gmail.com>
2026-02-28 06:32:26 +0700

    refactor : add other revenue table sorting latest to oldest(defaultsort)

--- app/Filament/Resources/OtherRevenueResource.php
+++ app/Filament/Resources/OtherRevenueResource.php
@@ -155,7 +155,8 @@ class OtherRevenueResource extends Resource
                 Tables\Actions\BulkActionGroup::make([
                     Tables\Actions\DeleteBulkAction::make(),
                 ]),
-            ]);
+            ])
+            ->defaultSort('created_at', 'desc');
     }
 
     public static function getRelations(): array
